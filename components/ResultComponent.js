fe5a5893d2c6c5ab6a4d7a387c97fcb3bebd67d9
Jehad Abu Awwad <jadaboawwad@gmail.com>
2023-05-02 21:15:57 +0300

    fix all

--- components/ResultComponent.js
+++ components/ResultComponent.js
@@ -8,8 +8,8 @@ export default function ResultComponent() {
     const hepatitis_api = process.env.NEXT_PUBLIC_API_URL_2;
     let userEmail = "";
     if (typeof window !== "undefined") {
-        userEmail = JSON.parse(localStorage.getItem("Auth")).email;
-    }
+    userEmail = JSON.parse(localStorage.getItem("Auth")).email;
+}
     let [resultsSection, setResultsSection] = useState(0)
     let [strokeResultData, setStrokeResultData] = useState([])
     let [hepatitisResultData, setHepatitisResultData] = useState([])
@@ -17,73 +17,69 @@ export default function ResultComponent() {
 
     useEffect(() => {
         axios
-        .get(stroke_api)
-        .then(result=>{
-            setStrokeResultData(result.data)
-        })
-        .catch(error=>{
-            console.log(error)
-        })
+            .get(stroke_api)
+            .then(result => {
+                setStrokeResultData(result.data)
+            })
+            .catch(error => {
+                console.log(error)
+            })
 
         axios
-        .get(hepatitis_api)
-        .then(result=>{
-            setHepatitisResultData(result.data)
-        })
-        .catch(error=>{
-            console.log(error)
-        })
+            .get(hepatitis_api)
+            .then(result => {
+                setHepatitisResultData(result.data)
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }, []);
 
 
-    if(resultsSection == 0)
-    {
-        testCount = 0;
-        return (
-        <>
-        <div class="w-10/12 md:w-7/12 lg:6/12 mx-auto relative py-20">
-          {strokeResultData.length > 0 ?<>
-            <h1 class="text-3xl text-center font-bold text-black-500">Your Test Results:</h1>
-            <div class="border-l-2 mt-10">
-            <ChoiceRadio setResultsSection={setResultsSection} />
-            {strokeResultData.map(result=>{
-                    if(result.email == userEmail)
-                    {
-                        testCount++;
-                        return (
-                            <TestResult result={result} testNumber={testCount}/>
-                        )
-                    }
-                })}
-            </div>
-          </> : <h1 class="text-3xl text-center font-bold text-black-500">No Test Results To Be Viewed Yet.</h1>}
-        </div>
-        </>
-        );
-    }
-    else if (resultsSection == 1)
-    {
+    if (resultsSection == 0) {
         testCount = 0;
         return (
             <>
-            <div class="w-10/12 md:w-7/12 lg:6/12 mx-auto relative py-20">
-              {hepatitisResultData.length > 0 ?<>
-                <h1 class="text-3xl text-center font-bold text-black-500">Your Test Results:</h1>
-                <div class="border-l-2 mt-10">
-                <ChoiceRadio setResultsSection={setResultsSection} />
-                {hepatitisResultData.map(result=>{
-                        if(result.email == userEmail)
-                        {
-                            testCount++;
-                            return (
-                                <TestResult result={result} testNumber={testCount}/>
-                            )
-                        }
-                    })}
+                <div class="w-10/12 md:w-7/12 lg:6/12 mx-auto relative py-20">
+                    {strokeResultData.length > 0 ? <>
+                        <h1 class="text-3xl text-center font-bold text-black-500">Your Test Results:</h1>
+                        <div class="border-l-2 mt-10">
+                            <ChoiceRadio setResultsSection={setResultsSection} />
+                            {strokeResultData.map(result => {
+                                if (result.email == userEmail) {
+                                    testCount++;
+                                    return (
+                                        <TestResult result={result} testNumber={testCount} />
+                                    )
+                                }
+                            })}
+                        </div>
+                    </> : <h1 class="text-3xl text-center font-bold text-black-500">No Test Results To Be Viewed Yet.</h1>}
                 </div>
-              </> : <h1 class="text-3xl text-center font-bold text-black-500">No Test Results To Be Viewed Yet.</h1>}
-            </div>
             </>
-            );
+        );
+    }
+    else if (resultsSection == 1) {
+        testCount = 0;
+        return (
+            <>
+                <div class="w-10/12 md:w-7/12 lg:6/12 mx-auto relative py-20">
+                    {hepatitisResultData.length > 0 ? <>
+                        <h1 class="text-3xl text-center font-bold text-black-500">Your Test Results:</h1>
+                        <div class="border-l-2 mt-10">
+                            <ChoiceRadio setResultsSection={setResultsSection} />
+                            {hepatitisResultData.map(result => {
+                                if (result.email == userEmail) {
+                                    testCount++;
+                                    return (
+                                        <TestResult result={result} testNumber={testCount} />
+                                    )
+                                }
+                            })}
+                        </div>
+                    </> : <h1 class="text-3xl text-center font-bold text-black-500">No Test Results To Be Viewed Yet.</h1>}
+                </div>
+            </>
+        );
     }
 }
